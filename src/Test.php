1a937f49107f99b8f415f4aa639c7249fa0da6f7
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-15 00:00:42 +0900

    initial commit

---
+++ src/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+class Test
+{
+    public $test = 0;
+
+    public function echoTest()
+    {
+        echo 'test';
+        echo 'aiuo';
+        echo 'aiueoijdfjdkfjdf';
+        echo 'aiuifidf';
+    }
+}
